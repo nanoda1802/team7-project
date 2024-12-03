5813dbc6508b2b2eb6cf08ce18f913ca006bb64c
ohhyeseong <ohs020105@gmail.com>
2024-12-03 20:22:36 +0900

    [fix] 다시 저장

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from "express";
+import Cash from "./routes/cash-recharge-router.js";
+// import character from "./routes/character_routers.js";
 import cookieParser from "cookie-parser";
 
 /* express 생성 */
@@ -10,7 +12,7 @@ app.use(express.json()); // 바디 파서
 app.use(cookieParser()); // 쿠키 파서
 
 /* 라우터 경로 배정 */
-app.use("/api", []);
+app.use("/api", [Cash]);
 
 /* 서버 오픈 알리미 */
 app.listen(PORT, () => {
---
+++ src/routes/character_routers.js
@@ -0,0 +1,27 @@
+// import express from "express";
+// import { prisma } from "../utils/prisma/index.js";
+
+// const router = express.Router();
+
+// // 강화 라우터
+// router.patch("/api/users/:key/agents/intensify");
+
+// const { key } = req.params;
+// const { agentkey } = req.body;
+
+// // 로그인 상태인지 아닌지 확인
+// if (!checkLogin(req)) {
+//   return resizeBy.status(401).json({ message: "로그인 부터 해주세요!!" });
+// }
+// // 로그인 된 계정의 아이디가 아닌 경우 거절
+// const loggedlnUser = getUserid(req);
+// if (loggedlnUser !== userID) {
+//   return resizeBy.status(403).json({ message: "당신 계정이 아녀요!!"});
+// }
+
+// try {
+//   // 보유 선수 확인
+//   const plater = await prisma.$queryRaw()
+// }
+
+// export default router;
