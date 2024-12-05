f144856b93b0a36900cc9729c3d030c155767374
조용필 <78397393+bbie-6772@users.noreply.github.com>
2024-12-05 12:42:10 +0900

    [Refactor] 스쿼드 점수 계산 + 정규전 리팩토링
    스쿼드 점수의 일부 로직을 간략화함 +
    정규전 API에 사용하는 함수를 외부에서 내부로 이동시켜 가독성을 높였으며, 일부 로직들을 친선전처럼 간소화 하였음

--- src/routes/match-router.js
+++ src/routes/match-router.js
@@ -7,11 +7,11 @@ import { check } from "prisma";
 /* 계정 라우터 생성 */
 const router = express.Router();
 
-/* 스쿼드 종합 점수 계산 */
+/* 스쿼드 종합 점수 계산 함수 */
 const checkSquadScore = async (user) => {
   let squadScore = 0;
   // [1] 스쿼드 구성 멤버 확인
-  const { squadMem1, squadMem2, squadMem3, synergy } = user
+  const { squadMem1, squadMem2, squadMem3, synergy} = user
   const checkSquad = [ squadMem1, squadMem2, squadMem3] 
 
   // [2] 구성 멤버 스탯에 강화 및 돌파 수치 적용
@@ -60,14 +60,18 @@ const checkSquadScore = async (user) => {
 
   }
   // [3] 팀 시너지 적용 여부 판단
-  checkSquad.synergy !== "none" ? (squadScore *= 1.1) : squadScore;
+  synergy !== "none" ? (squadScore *= 1.1) : squadScore;
   // [4] 소수점 날린 팀 스코어 반환
   return Math.trunc(squadScore);
 };
 
+/* 경기 결과 적용 */
+const applyMatchResult = async (matchResult, winner, loser) => {
+  
+};
+
 /* 친선전 API */
 router.post("/users/:key/select-match", authMiddleware, async (req, res, next) => {
-  const { key } = req.params; // 매개 경로변수에서 내 userKey 받음
   // 인증 미들웨어 거쳐서도 내 키 받음
   const { counterpart } = req.body; // body에서 상대방 아이디 수령
   // [검사 authMW] : 로그아웃 상태면 거부
@@ -98,14 +102,12 @@ router.post("/users/:key/select-match", authMiddleware, async (req, res, next) =
   let matchResult = "";
   const score = Math.round(Math.random() * 100)
   const win = Math.round(myScore / (myScore + counterScore) * 100)
-  
-  console.log(score,win)
 
   if (score < win) {
     matchResult = "승리!!";
   } else if (score > win) {
     matchResult = "패배!!";
-  } else if (score === win) {
+  } else {
     matchResult = "무승부!!";
   }
 
@@ -115,118 +117,161 @@ router.post("/users/:key/select-match", authMiddleware, async (req, res, next) =
     .json({ message: `나의 팀 점수 ${myScore}점, 상대 팀 점수 ${counterScore}점이지만.. ${matchResult}` });
 });
 
-/* 매치 메이킹 */
-const matchMaking = async (key) => {
-  // [1] 내 계정 mmr 찾기
-  const { mmr: myMatchRank } = await prisma.ranks.findFirst({
-    where: { userKey: key },
-    select: { mmr: true },
-  });
-  // [2] 내 mmr 그룹 찾기
-  const matchRanks = await prisma.ranks.findMany({
-    where: {
-      mmr: { gte: myMatchRank - 1000, lte: myMatchRank + 1000 },
-    },
-    select: { userKey: true, mmr: true },
-  });
-  // [3] mmr 그룹에서 본인과 팀편성 미비 유저 제외하고 다시 그루핑? 하츄핑?
-  const myRankGroup = await Promise.all(
-    matchRanks.map(async (user) => {
-      const { squadMem1, squadMem2, squadMem3 } = await prisma.users.findFirst({
-        where: { userKey: user.userKey },
-      });
-      // [3-1] 팀편성이 돼있고, 내 계정이 아닌 경우만 살려줌
-      return squadMem1 && squadMem2 && squadMem3 && user.userKey !== key ? user : false;
-    })
-  ).then((users) => users.filter((user) => user)); // [3-2] 살아남은 넘만 필터링
-  // [4] 그룹 중 한 명 랜덤으로 선정!!
-  const counterpart = myRankGroup[Math.trunc(Math.random() * myRankGroup.length)].userKey;
-  // [5] 상대할 유저의 userKey 반환
-  return counterpart;
-};
-
-/* 경기 결과 적용 */
-const applyMatchResult = async (matchResult, winner, loser) => {
-  // [1] 승, 패, 무 분기별로 트랜잭션 ON
-  if (matchResult !== "무승부!!") {
-    // [2] 승패가 갈렸을 때
-    await prisma.$transaction(async (tx) => {
-      // [2-1] Ranks 테이블에서, 승자 winCount 1 증가하고 mmr 50 증가
-      await tx.ranks.update({
-        where: { userKey: winner },
-        data: { winCount: { increment: 1 }, mmr: { increment: 50 } },
-      });
-      // [2-2] Ranks 테이블에서, 패자 loseCount 1 증가하고 mmr 20 감소
-      await tx.ranks.update({
-        where: { userKey: loser },
-        data: { loseCount: { increment: 1 }, mmr: { decrement: 20 } },
-      });
-      // [2-3] Assets 테이블에서, 승자 enhancer 10 증가하고 cash 50,000 증가
-      await tx.assets.update({
-        where: { userKey: winner },
-        data: { enhancer: { increment: 10 }, cash: { increment: 50000 } },
-      });
-      // [2-3] Assets 테이블에서, 패자 enhancer 2 증가하고 cash 10,000 증가
-      await tx.assets.update({
-        where: { userKey: loser },
-        data: { enhancer: { increment: 2 }, cash: { increment: 10000 } },
-      });
-    });
-  } else if (matchResult === "무승부!!") {
-    // [3] 비겼을 때
-    await prisma.$transaction(async (tx) => {
-      // [3-1] Ranks 테이블에서, 둘다 drawCount 1 증가하고 mmr 5 증가
-      await tx.ranks.updateMany({
-        where: { userKey: { in: [winner, loser] } },
-        data: { drawCount: { increment: 1 }, mmr: { increment: 5 } },
-      });
-      // [3-2] Assets 테이블에서, 둘다 enhancer 5 증가하고 cash 30,000 증가
-      await tx.assets.updateMany({
-        where: { userKey: { in: [winner, loser] } },
-        data: { enhancer: { increment: 5 }, cash: { increment: 30000 } },
-      });
-    });
-  }
-};
-
 /* 정규전 API */
-router.post("/users/:key/rank-match", async (req, res, next) => {
+router.post("/users/:key/rank-match", authMiddleware, async (req, res, next) => {
   const { key } = req.params; // 매개 경로변수에서 내 userKey 받음
   // 인증 미들웨어 거쳐서도 내 키 받음
   // [검사 authMW] : 로그아웃 상태면 거부
   // [검사 authMW] : 로그인 정보와 아이디 불일치 시 거부
   // [검사 01] : 팀편성 안 됐을 시 거부
-  const { squadMem1, squadMem2, squadMem3 } = await prisma.users.findFirst({
-    where: { userKey: +key },
-  });
-  if (!(squadMem1 && squadMem2 && squadMem3)) return res.status(401).json({ message: "팀편성부터 하세요라!!" });
+  const { squadMem1, squadMem2, squadMem3 } = req.user
+  if (!(squadMem1 && squadMem2 && squadMem3)) return res
+    .status(401)
+    .json({ message: "팀편성부터 하세요라!!" });
+
   // [1] 매치 메이킹
-  const counterKey = await matchMaking(+key);
+  // [1-1] 내 계정 mmr 찾기
+  const { mmr: myMatchRank } = await prisma.ranks.findFirst({
+    where: { userKey: +key },
+    select: { mmr: true },
+  });
+  // [1-2] 내 mmr 그룹 찾기
+  const matchRanks = await prisma.ranks.findMany({
+    where: {
+      mmr: { gte: myMatchRank - 1000, lte: myMatchRank + 1000 },
+    },
+    select: { 
+      mmr: true,
+      user: true
+    },
+  });
+  // [1-3] mmr 그룹에서 본인과 팀편성 미비 유저 제외하고 다시 그루핑? 하츄핑?
+  // [1-3-1] 팀편성이 돼있고, 내 계정이 아닌 경우만 살려줌
+  const myRankGroup = matchRanks.filter((e) => e.user.squadMem1 && e.user.squadMem2 && e.user.squadMem3 && e.user.userKey !== key)
+  
+  if (!myRankGroup) return res
+    .status(404)
+    .json({errorMessage: "적절한 상대를 찾을 수 없습니다"})
+  
+  // [1-4] 그룹 중 한 명 랜덤으로 선정!!
+  const counterUser = myRankGroup[Math.trunc(Math.random() * myRankGroup.length)].user
+
   // [2] 스코어 비교해 경기 결과 계산
-  const myScore = await checkSquadScore(+key);
-  const counterScore = await checkSquadScore(counterKey);
+  const myScore = await checkSquadScore(req.user);
+  const counterScore = await checkSquadScore(counterUser);
+
   // [3] 승, 패, 무 분기 별로 보상 및 점수 변동 적용
   let matchResult = "";
-  if (myScore > counterScore) {
+  const score = Math.round(Math.random() * 100)
+  const win = Math.round(myScore / (myScore + counterScore) * 100)
+
+  if (score < win) {
+    await updateData(req.user.userKey, counterUser.userKey)
     matchResult = "승리!!";
-    applyMatchResult(matchResult, +key, counterKey);
-  } else if (myScore < counterScore) {
+    
+  } else if (score > win) {
+    await updateData(counterUser.userKey, req.user.userKey)
     matchResult = "패배!!";
-    applyMatchResult(matchResult, counterKey, +key);
-  } else if (myScore === counterScore) {
+  } else {
+    // [3] 비겼을 때
+    // [3-1] Ranks 테이블에서, 둘다 drawCount 1 증가하고 mmr 5 증가
+    // [3-2] Assets 테이블에서, 둘다 enhancer 5 증가하고 cash 30,000 증가
+    await prisma.$transaction(async (tx) => {
+      await tx.ranks.updateMany({
+        where: {
+          OR: [
+            { userKey: +key },
+            { userKey: counterUser.userKey }
+          ]
+        },
+        data: {
+          drawCount: { increment: 1 },
+          mmr: { increment: 5 }
+        }
+      });
+      await tx.assets.updateMany({
+        where: {
+          OR: [
+            { userKey: +key },
+            { userKey: counterUser.userKey }
+          ]
+        },
+        data: {
+          enhancer: { increment: 5 },
+          cash: { increment: 30000 }
+        }
+      });
+
+    })
     matchResult = "무승부!!";
-    applyMatchResult(matchResult, +key, counterKey);
   }
+
+  // 승패 유무에 따른 보상 업데이트 함수
+  async function updateData (win,lose) {
+    // [1] 승, 패, 무 분기별로 트랜잭션 ON
+    // [2] 승패가 갈렸을 때
+    await prisma.$transaction(async (tx) => {
+      // [2-1] 승자 Ranks 테이블에서, 승자 winCount 1 증가하고 mmr 50 증가
+      // [2-2] 승자 Assets 테이블에서, 승자 enhancer 10 증가하고 cash 50,000 증가
+      await tx.users.update({
+        where: { userKey: win },
+        data: {
+          rank: {
+            update: {
+              data: {
+                winCount: { increment: 1 },
+                mmr: { increment: 50 }
+              }
+            }
+          },
+          asset: {
+            update: {
+              data: {
+                enhancer: { increment: 10 },
+                cash: { increment: 50000 }
+              }
+            }
+          }
+        },
+      });
+      // [2-3] 패자 Ranks 테이블에서, 패자 loseCount 1 증가하고 mmr 20 감소
+      // [2-4] 패자 Assets 테이블에서, 패자 enhancer 2 증가하고 cash 10,000 증가
+      await tx.users.update({
+        where: { userKey: lose },
+        data: {
+          rank: {
+            update: {
+              data: {
+                loseCount: { increment: 1 },
+                mmr: { decrement: 20 }
+              }
+            }
+          },
+          asset: {
+            update: {
+              data: {
+                enhancer: { increment: 2 },
+                cash: { increment: 10000 }
+              }
+            }
+          }
+        },
+      });
+    });
+
+    return 
+  }
+
   // [4] 각 팀의 스코어와 경기 결과 응답
   return res
     .status(201)
-    .json({ message: `나의 팀 점수 ${myScore}점, 상대 팀 점수 ${counterScore}점으로 ${matchResult}` });
+    .json({ message: `나의 팀 점수 ${myScore}점, 상대 팀 점수 ${counterScore}점이지만.. ${matchResult}` });
 });
 
 // 랭킹 조회 api
 router.get('/users/ranks', async (req, res, next) => {
+  //출력용 json
   let resJson = [];
-
   const ranking = await prisma.ranks.findMany({
     orderBy: [{
       mmr: 'desc',
@@ -245,9 +290,7 @@ router.get('/users/ranks', async (req, res, next) => {
   })
 
   for (let i = 0; i < ranking.length; i++) {
-
     const winningRate = Math.round(ranking[i].winCount / (ranking[i].winCount + ranking[i].loseCount + ranking[i].drawCount) * 100) || 0
-
     resJson = [...resJson,
     {
       rank: i + 1,
@@ -263,7 +306,6 @@ router.get('/users/ranks', async (req, res, next) => {
   return res
     .status(200)
     .json(resJson)
-
 })
 
 // 팀편성 API
