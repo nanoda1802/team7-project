bd710e7c9ad47dceec15946d9d28f948377b25b3
이이삭 <dlrltn1091@gmail.com>
2024-12-06 17:18:47 +0900

    frontend fix
    진짜 끝

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ import matchRouter from "./routes/match-router.js";
 import userRouter from "./routes/user-router.js";
 import path from "path";
 import cors from 'cors';
+import { fileURLToPath } from "url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));  
+const __filename = fileURLToPath(import.meta.url); 
 
 /* express 생성 */
 const app = express();
@@ -27,6 +31,7 @@ app.use(cors({
 app.use(express.json()); // 바디 파서
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/",(req,res)=>{return res.sendFile(path.join(publicPath , "mainpage.html"))});
 /* 라우터 경로 배정 */
 app.use("/api", [agentsRouter, matchRouter, userRouter]);
 
